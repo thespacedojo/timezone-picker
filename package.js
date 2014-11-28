b4f059e8a40cf764f8a84e67df16f708bf341a89
Josh Owens <joshua.owens@gmail.com>
2014-11-27 22:29:57 -0500

    Relax meteor version back to 0.9.4

--- package.js
+++ package.js
@@ -1,14 +1,16 @@
 Package.describe({
-  summary: "Nicely formatted dropdown list of timezones, based on moment. Also timezone detection."
-  version: '0.1.0',
+  summary: "Nicely formatted dropdown list of timezones, based on moment. Also timezone detection.",
+  version: '0.1.1',
   name: "joshowens:timezone-picker",
   git: 'https://github.com/MeteorClub/timezone-picker'
 });
 
 Package.onUse(function(api, where) {
+  api.versionsFrom("METEOR@0.9.4");
+
   api.use(['blaze', 'templating', 'jquery'], 'client');
   api.use(['underscore'], 'client');
-  api.use(['mrt:moment', 'mrt:moment-timezone'], 'client');
+  api.use(['mrt:moment@2.8.1', 'mrt:moment-timezone@0.2.1'], 'client');
 
   api.addFiles([
     'mapping.js',
